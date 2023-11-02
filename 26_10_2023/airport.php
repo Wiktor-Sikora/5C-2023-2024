2af31687eb8b757d751b154fefc12fdf316f9ca7
Wiktor-Sikora <sikora.wiktor777@gmail.com>
2023-11-02 14:05:45 +0100

    lotnisko egzamin

--- 26_10_2023/airport.php
+++ 26_10_2023/airport.php
@@ -27,19 +27,25 @@
             </thead>
             <?php 
             
-            $con = mysqli_connect('localhost', 'root', '' , 'egzamin');
+                $con = mysqli_connect('localhost', 'root', '' , 'egzamin');
 
-            $zapytanie1 = "SELECT id, nr_rejsu, czas, kierunek, status_lotu FROM `odloty` ORDER BY czas DESC;";
+                $zapytanie1 = "SELECT id, nr_rejsu, czas, kierunek, status_lotu FROM `odloty` ORDER BY czas DESC;";
 
-            
+                $result = mysqli_query($con, $zapytanie1);
+
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_array($result)) {
+                        echo "<tr>";
+                        echo "<td>$row[0]</td>";
+                        echo "<td>$row[1]</td>";
+                        echo "<td>$row[2]</td>";
+                        echo "<td>$row[3]</td>";
+                        echo "<td>$row[4]</td>";
+                        echo "</tr>";
+                    }
+                }
+                mysqli_close($con);
             ?>
-            <tr>
-                <td>dupa</td>
-                <td>dupa</td>
-                <td>dupa</td>
-                <td>dupa</td>
-                <td>dupa</td>
-            </tr>
         </table>
     </main>
     <footer>
@@ -47,7 +53,16 @@
             <a href="kw1.jpg" target="_blank">Pobierz obraz</a>
         </div>
         <div>
-            <p>dupa</p>
+            <p>
+            <?php
+                if (!isset($_COOKIE["cookie"])) {
+                    setcookie("cookie", "cookie", time() + 3600, "/");
+                    echo "<i>Dzień dobry! Sprawdź regulamin naszej strony</i>";
+                } else {
+                    echo "<i>Miło nam, że nas znowu odwiedziłeś</i>";
+                }
+            ?>
+            </p>
         </div>
         <div>
             Autor: Wiktor Sikora
